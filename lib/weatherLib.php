9e7bee1c1b77a1f6e4b6d1b437811c37b3ea1c91
GRISHNOV <grishnov98@mail.ru>
2020-04-09 02:55:17 +0300

    error logging with invalid token

--- lib/weatherLib.php
+++ lib/weatherLib.php
@@ -1,9 +1,14 @@
 <?php
 
-    define('OPENWEATHER_API_TOKEN', '********************************');
+    define('OPENWEATHER_API_TOKEN', '********************************'); // Confidential information
 
     class Weather{
         public static function getWeather($city) {
+            if(OPENWEATHER_API_TOKEN == '********************************'){
+                echo "System failure, no token specified for OpenWeather!";
+                trigger_error("System failure, no token specified for OpenWeather!", E_USER_ERROR);
+                exit("System failure, no token specified for OpenWeather!");
+            }
             $request_url = "http://api.openweathermap.org/data/2.5/weather?q=" . $city . "&appid=" .
                 OPENWEATHER_API_TOKEN . "&units=metric";
             $weather_request = file_get_contents($request_url);
