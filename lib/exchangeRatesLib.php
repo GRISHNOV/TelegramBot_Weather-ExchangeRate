3e354282b155c436c8db0ecf2f181d8b22b4ff14
GRISHNOV <grishnov98@mail.ru>
2020-04-11 23:09:27 +0300

    adding processing exceptions when the exchange rate service fails

--- lib/exchangeRatesLib.php
+++ lib/exchangeRatesLib.php
@@ -2,7 +2,15 @@
 
     class ExchangeRates{
         public static function getExchangeRates($currency) {
-            $rates_request = file_get_contents('https://www.cbr-xml-daily.ru/daily_json.js');
+            try {
+                $rates_request = file_get_contents('https://www.cbr-xml-daily.ru/daily_json.js');
+                if ($rates_request == false){
+                    throw new Exception('It is not possible to connect to the exchange rate API');
+                }
+            } catch (Exception $e) {
+                echo "System failure: " . $e->getMessage();
+                return "API_REQUEST_FAILURE";
+            }
             $rates_value = json_decode($rates_request, true);
             return Array(
                 "CurrentValue" => $rates_value['Valute'][$currency]['Value'],
